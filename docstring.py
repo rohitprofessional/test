b9058e4eb77712629ab0684a11205d42197e6dfb
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-03 14:59:48 +0530

    Update docstring.py

--- docstring.py
+++ docstring.py
@@ -2,7 +2,7 @@ def square(x):
     '''
     Takes a number n and returns   
     square of the number, i.e, n*n.
-    ''' # its different than comment. cimp
+    ''' # its different than comment. It can change program output but comments can't do that.
     print(x*x)
 
 square(5)
