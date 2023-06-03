384aa84c73a54d27c9fb7fc265912fd7621da097
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-03 14:58:00 +0530

    Add files via upload

---
+++ docstring.py
@@ -0,0 +1,9 @@
+def square(x):
+    '''
+    Takes a number n and returns   
+    square of the number, i.e, n*n.
+    ''' # its different than comment. cimp
+    print(x*x)
+
+square(5)
+print(square.__doc__)   # to print doc string use 'doc()' func.
