3f2f39a9c4d839d6d391b9deb8bdebd1442a8c95
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-03 12:24:49 +0530

    Add files via upload

--- Rock_Paper_Scissor.py
+++ Rock_Paper_Scissor.py
@@ -2,94 +2,134 @@
 
 import random
 
-Items = ['rock','paper','scissor']
+Items = ["rock","paper","scissor"]
 
 print("----------------------------------")
 print("This is a ROCK-PAPER-SCISSOR game")
 print("----------------------------------")
 
-print("Do you want to play the game?")
 
 while True:
-    Computer = random.choice(Items)
+    C_score = 0
+    G_score = 0
 
-    play = int(input('''Lets Play ! 
-                - Press 1 for play
-                - Press 2 for exit
-                Your choice -->>'''))
-    if play == 1:
-        Name = input('Please enter you name:')
-        print(Name,",","You have entered into the ROCK-PAPER-SCISSOR game!")
-        print("----------------------------------")
+    uc = int(input('''
+    Lets Play ! 
+    - Press 1 for play
+    - Press 0 for exit
+    Your choice :-->>'''))
+
+    if uc == 1:
+        
         print("-----------GAME STARTED-----------")
 
-        for i in range(0,6,1):
-            Gamer = int(input('''Choose you options: 
-                    - Press 1 for rock
-                    - Press 2 for paper
-                    - Press 3 for scissor
-                    Your option-->>'''))
+        for i in range(1,6):
+            User_input = int(input('''
+            Choose you options: 
+                - Press 1 for rock
+                - Press 2 for paper
+                - Press 3 for scissor
+                - Your option :-->>'''))
             
-            if(Gamer == 1 ):
-                print(Name,"your choice :",Gamer,"Rock")
-                print("Computer's choice :",Computer)
+            if User_input == 1 :
+                U_choice = Items[0]
+                print("your choice :","Rock")
+                print("Computer's choice :",C_choice)
 
-            elif(Gamer == 2):
-                print(Name,"your choice :",Gamer,"paper")
-                print("Computer's choice :",Computer)
+            elif User_input == 2 :
+                U_choice = Items[1]
+                print("your choice :","paper")
+                print("Computer's choice :",C_choice)
 
-            elif(Gamer == 3):
-                print(Name,"your choice :",Gamer,"scissor")
-                print("Computer's choice :",Computer)
+            elif User_input == 3 :
+                U_choice = Items[2]
+                print("your choice :","scissor")
+                print("Computer's choice :",C_choice)
 
             else:
                 print("Invalid choice")            
             
-            if(Computer == 1 & Gamer == 1 ):
-                print(Computer)
-                print(Gamer)
-                print("This round is draw.")
-            elif(Computer == 1 & Gamer == 2 ):
-                print(Computer)
-                print(Gamer)
-                print("Gamer wins.")
-            elif(Computer == 1 & Gamer == 3 ):
-                print(Computer)
-                print(Gamer)
-                print("Computer wins.")
-            
-            if(Computer == 2 & Gamer == 1 ):
-                print(Computer)
-                print(Gamer)
-                print("This round is draw.")
-            elif(Computer == 2 & Gamer == 2 ):
-                print(Computer)
-                print(Gamer)
-                print("Gamer wins.")
-            elif(Computer == 2 & Gamer == 3 ):
-                print(Computer)
-                print(Gamer)
-                print("Computer wins.")
-            
-            if(Computer == 3 & Gamer == 1 ):
-                print(Computer)
-                print(Gamer)
-                print("Gamer wins.")
-            elif(Computer == 3 & Gamer == 2 ):
-                print(Computer)
-                print(Gamer)
-                print("Computer wins.")
-            elif(Computer == 3 & Gamer == 3 ):
-                print(Computer)
-                print(Gamer)
-                print("This round is draw.")
+            C_choice = random.choice(Items)
 
-            pass
+            if C_choice == U_choice:
+                print("Computer value :",C_choice)
+                print("Computer value :",U_choice)
+                print("Game draws.")
+                print("computer",C_score)
+                print("user",G_score)
+                
+            elif (U_choice == "rock" and C_choice == "paper") or (U_choice 
+                 == "scissor" and C_choice == "rock") or (U_choice == "paper"
+                 and C_choice == "scissor"):
+                print("Computer wins")
+                C_score += 1
+                print("computer :",C_score)
+                print("user :",G_score)
+                pass
+
+            else: 
+                print("Gamer wins")
+                G_score += 1
+                print(C_score)
+                print(G_score)
+            
         else:
-            break
+            print("try another time.")
+            
+    print("Thank you. Come again next time.")
 
 
-print("Thank you. Come again next time.")
+
+# if(Computer == 1 & Gamer == 1 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("This round is draw.")
+            # elif(Computer == 1 & Gamer == 2 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Gamer wins.")
+            #     G_score += 1 
+            # elif(Computer == 1 & Gamer == 3 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Computer wins.")
+            #     C_score += 1
+            
+            # if(Computer == 2 & Gamer == 1 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Computer wins.")
+            #     G_score += 1
+            # elif(Computer == 2 & Gamer == 2 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("This round is draw.")
+            # elif(Computer == 2 & Gamer == 3 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Gamer wins.")
+            #     G_score += 1
+            
+            # if(Computer == 3 & Gamer == 1 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Gamer wins.")
+            #     G_score += 1
+            # elif(Computer == 3 & Gamer == 2 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("Computer wins.")
+            #     C_score += 1
+            # elif(Computer == 3 & Gamer == 3 ):
+            #     print(Computer)
+            #     print(Gamer)
+            #     print("This round is draw.")
+
+
+
+
+
+
 
 
 
