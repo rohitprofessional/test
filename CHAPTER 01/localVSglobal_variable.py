31188cfbb5491eba9d7c9e4f7bbd66942e3db214
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-10 22:25:16 +0530

    Update localVSglobal_variable.py

--- CHAPTER 01/localVSglobal_variable.py
+++ CHAPTER 01/localVSglobal_variable.py
@@ -1,4 +1,9 @@
-x = 10       # global variable and can be use anywhere in prog
+'''It is not advisable to update or change the global variable
+value within in a local block of code. As it can lead to complexity
+in the program.
+But python provides a global keyword to do so if you want to.'''
+
+x = 10      # global variable and can be use anywhere in prog
 y = 20      # global variable and can be use anywhere in prog
 
 print(f"global variabel : {x}")
@@ -6,13 +11,15 @@ print(f"global variabel : {y}")
 
 def local():
     global x    # changing global variable value by accessing it
-    x = 30       # using the word 'global'
+    x = 30      # using the word 'global'
 
     print(f"1.Global variable : {x}")
     
-    x = 1   # local variable and can be accessible within the block only
-    y = 2   # after the block ends variable scope/use out of block also ends   
-    
+    x = 1   
+    # local variable and can be accessible within the block only.
+    # after the block ends variable scope/use out of block also ends   
+    y = 2       
+
     print(f"2. local variable : {x}")    #local variable
     print(f"2. local variable : {y}")
 
