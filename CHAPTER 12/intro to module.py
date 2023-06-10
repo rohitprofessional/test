017d0f07d8e9ece82a5e4ab33c42169ea7c2721a
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-10 15:38:11 +0530

    Add files via upload

--- CHAPTER 12/intro to module.py
+++ CHAPTER 12/intro to module.py
@@ -1,5 +1,6 @@
 # WAY NO. 1 TO CALL A MODULE AND USE ITS FUNCTIONS
 '''--------------------------------------------------------------'''
+
 # import module01 # using user defined module, cotains various func.
 '''OR'''
 # import module01 as m  # using an alias(shortform) for module name.
@@ -9,10 +10,10 @@
 
 '''Calling the required func from the module we imported. And passing
 the user given arguments. Here we have to explicitly call every
-func we need in our program.'''
+func we need in our program. Eg.'''
 
 # print(module01.mult(a,b)) # calling func using module name
-# print(m.mult(a,b))    # calling func using module alias name.
+# print(m.mult(a,b))        # calling func using module alias name.
 '''-------------------------------------------------------------'''
 
 '''Now instead of calling every required func. from module just 
@@ -29,8 +30,9 @@ from module01 import * # Here we are importing all the function using '*'
 a = int(input("enter the 1st no.:"))  
 b = int(input("enter the 2nd no.:"))
 
-print(sum(a,b)) 
-# Now just calling func by its name without using module name with 
+# Now just calling func by its name without using module name with using
 # dot'.' symbol. Means we don't have to enter module name again and
 # again to call the required function.
+
+print(sum(a,b)) 
 print(mult(a,b))
--- CHAPTER 12/module01.py
+++ CHAPTER 12/module01.py
@@ -21,8 +21,23 @@ def rem(a,b):
     c = a % b
     return c
 
-# a = int(input("enter the 1st number:"))  
-# b = int(input("enter the 2nd number:"))
+if __name__ == "__main__":
+    
+    a = int(input("enter the 1st number:"))  
+    b = int(input("enter the 2nd number:"))
 
-# ans = mult(a,b)
-# print("product of 2 numbers:",ans)
+    ans = mult(a,b)
+    print("product of 2 numbers:",ans)
+
+''' agr hum iss main part ko chalana chahate h to iss program ko yhi
+ se execute krna hoga. lekin agr main k bahar ka jitna bhi program
+ bach gya h usko chalana h to iss file ko import krke vha se chalana
+ hoga jisse jo bhi main k bahar ka part h sirf vo vaha se execute
+ hoga na ki ye pura module ka program.
+'''
+
+# if we only want to run this main part we have to the run this
+# program from here only otherwise import this module and from 
+# there run this program named as module01 so it work as a 
+# module for the program in w/c it will be imported.
+    
