419c209304131ac3a037f907a60fbb0620b4b06b
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-13 23:30:05 +0530

    Update credit_Card_checker.py

--- PRACTICE/credit_Card_checker.py
+++ PRACTICE/credit_Card_checker.py
@@ -32,10 +32,62 @@ Step 5: Final result – If the final result is divisible by 10, the card number
 
 '''
 
-# program to check credit card or debit card no. is valid or not.
+#-------------------------------------------------------------------------------------------
+def checkSecondDigits(num):
+    length = len(num)
+    sum =  0
+    for i in range(length-2,-1,-2):
+      number = eval(num[i])
+      number = number * 2
+      if number > 9:
+          strNumber = str(number)
+          number = eval(strNumber[0]) + eval(strNumber[1])
+          sum += number
+      return sum
 
-card_no = input('''Enter the 16 digit credit card no. 
-and put a space after every 4 digit: ''')
+def odd_digits(num):
+    length = len(num)
+    sumOdd = 0
+    for i in range(length-1,-1,-2):
+        num += eval(num[i])
+    return sumOdd
+
+def c_length(num):
+    length = len(num)
+    if num >= 13 and num <= 16:
+    if num [0] == "4" or num [0] == "5" or num [0] == "6" or (num [0] == "3" and num [1] == "7"):
+        return True
+    else:
+        return False
+
+
+def main():
+    filename = input("What is the name of your input file? ")
+    infile= open(filename,"r")
+    cc = (infile.readline().strip())
+    print(format("Card Number", "20s"), ("Valid / Invalid"))
+    print("------------------------------------")
+    while cc!= "EXIT":
+        even = checkSecondDigits(num)
+        odd = odd_digits(num)
+        c_len = c_length(num)
+        tot = even + odd
+
+        if c_len == True and tot % 10 == 0:
+            print(format(cc, "20s"), format("Valid", "20s"))
+        else:
+            print(format(cc, "20s"), format("Invalid", "20s"))
+        num = (infile.readline().strip())
+
+main()
+#---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# program to check credit card or debit card no. is valid or not.
+'''
+card_no = input("Enter the 16 digit credit card no. and put a space after every 4 digit:" )
 card_number_withou_space = card_no.replace(" ","")
 number = card_no.split(" ")
 odd_set = ""
@@ -57,11 +109,5 @@ else:
     print("3.Enter the numerical credit number. ")
 
 print(card_number_withou_space)
+'''
 
-# if(i == 4):
-#     for o in range(0,len(card_no),2):
-#        odd_set.(card_no)
-#        print(odd_set)
-# else:
-#     print("Enter the correct numerical credit number. ")
-    
