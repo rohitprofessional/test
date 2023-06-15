c79f1bc923b5674dde68a4ad8423c4d7cccded69
rohitprofessional <53004162+rohitprofessional@users.noreply.github.com>
2023-06-15 23:51:09 +0530

    Update output_04.py

--- OUTPUT PROG/output_04.py
+++ OUTPUT PROG/output_04.py
@@ -1,4 +1,6 @@
 x = 45
 while x < 50:
     print(x)
-    break
+    break   # otherwise it will go into infinite loop as while condition will always be true.
+    # or we can take value of x as an input from the user. if user enter value of x greater
+    # than 50 program will executed and doesn't enter into while loop and print nothing.
